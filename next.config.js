182e3125f51997996b4982c3c24c57ba1ece1f42
codrum <landrum287@hotmail.com>
2022-11-21 14:14:52 -0600

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
 	assetPrefix: isProd ? '/venomhack.com/' : '',
+}
+
+module.exports = {
+	nextConfig,
+	// comment out while in dev
 	images: {
-		unoptimized: true,
+		unoptomized: 'true',
 	},
 }
