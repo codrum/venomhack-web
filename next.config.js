85a1ed37f601064d3c25716634de7c2a54edcbe0
codrum <landrum287@hotmail.com>
2022-11-20 23:15:40 -0600

    add imgix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
+	assetPrefix: isProd ? '/venomhack.com/' : '',
 }
 
-module.exports = nextConfig
+module.exports = {
+	nextConfig,
+	images: {
+		loader: 'imgix',
+		path: 'https://venomhack.imgix.net',
+	},
+}
