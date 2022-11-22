f6011f6a79bf7f2a961cc1b12d03f51a6261d700
codrum <landrum287@hotmail.com>
2022-11-21 18:24:34 -0600

    rm asset prefix

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-	assetPrefix: isProd ? '/venomhack.com/' : undefined,
+	// assetPrefix: isProd ? '/venomhack.com/' : undefined,
 	swcMinify: true,
 	reactStrictMode: true,
 	// comment out while in dev
 	images: {
 		loader: 'imgix',
-		path: 'https://stillwater-disc-golf.imgix.net',
+		path: 'https://venom-hack.imgix.net',
 	},
 }
