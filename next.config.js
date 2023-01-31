3a1639db8d75ae3c3bcf05fdaf22b2e6640c168c
codrum <landrum287@hotmail.com>
2023-01-31 17:19:15 -0600

    rm img opt

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = nextConfig
+module.exports = {
+	// assetPrefix: isProd ? '/venomhack.com/' : undefined,
+	swcMinify: true,
+	reactStrictMode: true,
+	// comment out while in dev
+	images: {
+		unoptimized: true,
+	},
+}
