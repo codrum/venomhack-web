862ab33c47453b70aeb248ebf51709528f2dfb3a
codrum <codrum@users.noreply.github.com>
2023-02-06 05:22:13 +0000

    deploy: d8eb2116d9405ad1d4af03b9789e65b1b069b811

---
+++ _next/static/chunks/pages/downloads-c62d614fde907d54.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[526],{6771:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/downloads",function(){return u(7688)}])}},function(n){n.O(0,[247,688,774,888,179],function(){return n(n.s=6771)}),_N_E=n.O()}]);
