cc2a360bbc084d8a0cf013eb64aaf5b3e792075f
codrum <landrum287@hotmail.com>
2023-02-07 18:16:50 -0600

    add google tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,11 +24,12 @@ const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
         });
 		`}
 			</Script>
-			<Script
-				async
-				src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5016075289889293'
-				crossOrigin='anonymous'
-			></Script>
+
+			<Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TF72X3K');`}</Script>
 			<Wat2DoThemeProvider>
 				<Component {...pageProps} />
 			</Wat2DoThemeProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { FontFamilies } from '@/components/head'
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
 	return (
@@ -9,6 +10,12 @@ export default function Document() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<body>
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TF72X3K"
+						height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+					}}
+				/>
 				<Main />
 				<NextScript />
 			</body>
